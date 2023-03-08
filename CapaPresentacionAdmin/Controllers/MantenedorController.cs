c04e16bd897938b4d71b31c2f521933fc891e90d
krlCO <carloscerquinoropeza@gmail.com>
2023-03-07 23:48:12 -0500

    Descripcion - Marca

--- CapaPresentacionAdmin/Controllers/MantenedorController.cs
+++ CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -24,5 +24,85 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
+
+        //--Categoria--\\
+
+        [HttpGet]
+        public JsonResult ListarCategorias()
+        {
+            List<Categoria> olist = new List<Categoria>();
+            olist = new CN_Categoria().Listar();
+            return Json(new {data = olist}, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult RegistrarCategoria(Categoria cat)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (cat.IdCategoria == 0)
+            {
+                resultado = new CN_Categoria().Registrar(cat, out mensaje);
+
+            }
+            else
+            {
+                resultado = new CN_Categoria().Editar(cat, out mensaje);
+            }
+            return Json(new {resultado = resultado, mensaje = mensaje}, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarCategoria(int id)
+        {
+            bool rpta = false;
+            string mensaje = string.Empty;
+
+            rpta = new CN_Categoria().Eliminar(id, out mensaje);
+
+            return Json(new {result = rpta, mensaje = mensaje}, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public JsonResult ListarMarca()
+        {
+            List<Marca> oLista = new List<Marca>();
+            oLista = new CN_Marca().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult RegistrarMarca(Marca obj)
+        {
+            object result;
+            string mensaje = string.Empty;
+
+            if(obj.IdMarca == 0)
+            {
+                result = new CN_Marca().Registrar(obj, out mensaje);
+            }
+            else
+            {
+                result = new CN_Marca().Editar(obj, out mensaje);
+            }
+            return Json(new {result = result, mensaje = mensaje}, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult EliminarMarca(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Marca().Eliminar(id, out mensaje);  
+
+            return Json(new {resultado = respuesta, mensaje = mensaje}, JsonRequestBehavior.AllowGet);  
+        }
+
+
     }
 }
